cd299e59f006f596290a7ce1d561e75856489b2c
Edward Koome <koomeedward77@gmail.com>
2020-08-24 13:58:41 +0300

    add a form for quote entry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HeadingComponent } from './heading/heading.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HeadingComponent
+    HeadingComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
