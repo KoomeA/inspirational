3b91c8cbbeb2df5122a339544372689d0b76feda
Edward Koome <koomeedward77@gmail.com>
2020-08-24 14:50:02 +0300

    add input properties to the class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,7 @@
 export class Quote {
+
+    public quote: string;
+    public author: string;
+    public submitted: string;
+
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,7 +8,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
-
   
 
   constructor() { }
