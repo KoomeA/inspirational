d63c5787dc0d0e4fc2eda30622f6385d4e8bd39f
Edward Koome <koomeedward77@gmail.com>
2020-08-25 08:59:31 +0300

    add a readme

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Quote } from '@angular/compiler';
+import { Component, OnInit, Input, Output, EventEmitter  } from '@angular/core';
 
 @Component({
   selector: 'app-quote-details',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
+
+  @Input ()
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  quoteDelete(complete: boolean){
+    this.isComplete.emit(complete)
+  }
+  
 
   constructor() { }
 
